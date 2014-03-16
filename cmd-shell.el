bddd5b709300fb27810ba9e63161aa792e5adcbd
Jeremy Johnson <j@point-free.org>
2014-03-15 21:36:49 -0700

    type prompting

--- cmd-shell.el
+++ cmd-shell.el
@@ -1,3 +1,24 @@
+(defun cmd-shell-prompt-input (type)
+  (funcall (gethash type cmd-shell-input-forms
+                    (lambda ()
+                      (read-from-minibuffer "Text input: ")))))
+
+(defun cmd-shell-prompt-create-input (cmd-str)
+  ;; Use null string as escaped % for simplicity of implementation.
+  (let ((start 0)
+        (built-str "")
+        (cmd-str (replace-regexp-in-string "%%" "\0" cmd-str)))
+    (while (string-match "%\\(\\w+\\)" cmd-str start)
+      (let ((mstart (match-beginning 0))
+            (mend (match-end 0))
+            (type (match-string 1 cmd-str)))
+        (setq built-str (concat built-str (substring cmd-str start mstart)))
+        (setq built-str (concat built-str
+                                (cmd-shell-prompt-input (match-string 1 cmd-str))))
+        (setq start mend)))
+    (setq built-str (concat built-str (substring cmd-str start)))
+    (replace-regexp-in-string "\0" "%" built-str)))
+
 (defun cmd-shell-fetch-current-cmd ()
   (interactive)
   (save-excursion
@@ -22,11 +43,11 @@
       (setq start-of-cmd (point))
       (buffer-substring start-of-cmd end-of-cmd))))
 
-(defun cmd-shell-send-lines (str)
+(defun cmd-shell-send-lines (cmd-str)
   (with-current-buffer cmd-shell-shell
     (goto-char (point-max))
     (comint-kill-input)
-    (let ((lines (split-string str "\n")))
+    (let ((lines (split-string cmd-str "\n")))
       (mapcar (lambda (line)
                 (goto-char (point-max))
                 (insert line)
@@ -35,7 +56,7 @@
 
 (defun cmd-shell-send-input ()
   (interactive)
-  (let ((cmd-str (cmd-shell-fetch-current-cmd))
+  (let ((cmd-str (cmd-shell-prompt-create-input (cmd-shell-fetch-current-cmd)))
         (process (get-buffer-process cmd-shell-shell)))
     (if (not (string= cmd-str ""))
         (cmd-shell-send-lines cmd-str))))
@@ -54,6 +75,21 @@
                (forward-line)))
             (t (newline)))
     (newline)))
+
+(setq cmd-shell-input-forms (make-hash-table :test 'equal))
+
+(let ((git-branches
+       (lambda ()
+         (completing-read
+          "Branch: "
+          (delete "" (split-string
+                      (shell-command-to-string "git branch | sed 's/^[* ] //'")
+                      "\n"))
+          nil t))))
+  (puthash "branch" git-branches cmd-shell-input-forms)
+  (puthash "b" git-branches cmd-shell-input-forms)
+  (puthash "file" 'ido-find-file cmd-shell-input-forms)
+  (puthash "f" 'ido-find-file cmd-shell-input-forms))
 
 (define-derived-mode cmd-shell-mode text-mode "Cmd Shell"
   "Cmd shell mode
